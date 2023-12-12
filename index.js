60b8ff78e5ea182257c687b1301b802398e2988f
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2023-12-12 11:15:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<=400) {
+    return 'This one is on me!';
+  }else if(ride>2000){
+    if(ride>2500) {
+      return 'No can do.';
+    }
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city=='NYC' ? 'Ok, sounds good.': 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+      case 'not as generous':
+        return 'Thank you.'
+        break;
+        default:
+        return 'Bye.'
+        break;
+  }
 }
